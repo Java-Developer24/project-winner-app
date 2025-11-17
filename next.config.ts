842b76eec4b6662f99c6cbf8186cc8aa74019666
polagouni5301 <polagounibalakrishna@gmail.com>
2025-11-17 14:11:14 +0530

    cmd

--- next.config.ts
+++ next.config.ts
@@ -22,10 +22,10 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
